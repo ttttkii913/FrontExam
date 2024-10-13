686a175e80de0bc8fbadd7d11c337648cac1e982
ttttkii913 <dbgkdud821@daum.net>
2024-10-14 03:44:07 +0900

    2024_04_24_vue

---
+++ 2024_04_24_vue/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ExamList3 from './ExamList3.vue';
+</script>
+
+<template>
+  <div id="app">
+    <ExamList3 />
+  </div>
+</template>
+
+<style scoped>
+div#app { padding: 30px; margin: 30px auto; max-width: 600px; 
+  border: 1px solid #ccc; box-shadow: 3px 3px 3px #ddd;  }
+</style>
