f22cc49d3a9cf0bcd8d0716ebc04f6d848f5ba09
ttttkii913 <dbgkdud821@daum.net>
2024-11-18 01:28:04 +0900

    2024_05_22_router

---
+++ 2024_05_22/src/views/Exam1View.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps(['title', 'step']);
+const count = ref(0);
+const inputValue = ref(props.step); // 초기값을 props.step으로 설정
+</script>
+
+<template>
+  <div id="app">
+    <h1>Exam1View</h1>
+    <p>{{ count }}</p>
+    <br>
+      <input type="text" v-model="inputValue" class="text"/>
+      <button type="button" v-on:click="count += Number(inputValue)">++</button>
+      <button type="button" v-on:click="count -= Number(inputValue)">--</button>
+  </div>
+</template>
+
+<style scoped>
+  div { padding: 1em; text-align: center; 
+     border: 1px solid #aca; }
+
+  #app { 
+  width: 350px; min-height: 200px;
+  margin: 1em auto; padding: 1em;
+  border: 1px solid gray;
+  box-shadow: 3px 3px 3px #ddd;
+}
+
+
+button {
+  padding: 0.5em 2em;
+  margin: 3px;
+}
+p {
+  color: blue;
+  font-size: 30px;
+}
+input {
+  padding: 7px;
+  margin-right: 5px;
+  width: 50px;
+}
+</style>
---
+++ 2024_05_22/src/views/Exam2View.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue';
+const isChecked = ref(false); 
+const isChecked2 = ref(false);
+</script>
+
+<template>
+  <div id="app">
+    <h1>Exam2View</h1>
+      <input type="checkbox" v-model="isChecked" />
+      <label>체크 1</label>
+      <input type="checkbox" v-model="isChecked2" />
+      <label>체크 2</label>
+    <br />
+    
+    <button :disabled="!isChecked">버튼1</button>
+
+    <button :disabled="!isChecked2">버튼2</button>
+
+  </div>
+</template>
+
+<style scoped>
+  div { padding: 1em; text-align: center; 
+     border: 1px solid #aca; }
+  p { font-size: 20pt; text-align: center; margin: 0; }
+  #app { 
+  width: 350px; min-height: 200px;
+  margin: 1em auto; padding: 1em;
+  border: 1px solid gray;
+  box-shadow: 3px 3px 3px #ddd;
+}
+label { margin-right: 30px;}
+input {
+  margin-right: 5px;
+}
+button {
+  padding: 10px 20px;
+  margin-top: 10px;
+  margin-right: 20px;
+}
+</style>
